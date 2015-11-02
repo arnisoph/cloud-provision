d3cae6db131292115ba74dbcd429622d73b3c41b
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-11-02 17:33:19 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='vmcli',
+      version='0.0.1',
+      author='Arnold Bechtoldt',
+      author_email='mail@arnoldbechtoldt.com',)
